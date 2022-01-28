8229fc401547738b8ac86a260247926aa86d62c3
Arnaud Bétrémieux <arnaud@btmx.fr>
2022-01-28 16:26:17 +0100

    Fix build by removing components/home

--- src/components/home/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { RouteComponentProps } from 'react-router'
-import { APP_ROUTES } from 'components/App'
-
-export const Home: React.FC<RouteComponentProps> = props => {
-  props.history.replace(APP_ROUTES.INDEX)
-  return null
-}
